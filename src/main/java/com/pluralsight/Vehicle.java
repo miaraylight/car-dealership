df56e04154ead8774d64e980fb16fa043a523dcc
miaraylight <miaraylight@gmail.com>
2025-10-21 14:25:09 -0500

    add Vehicle.java

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,23 @@
 package com.pluralsight;
 
 public class Vehicle {
+    public int vin;
+    public int year;
+    public String make;
+    public String model;
+    public String vehicleType;
+    public String color;
+    public int odometer;
+    public double price;
+
+    public Vehicle(int vin, int year, String make, String model, String vehicleType, String color, int odometer, double price) {
+        this.vin = vin;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.odometer = odometer;
+        this.price = price;
+    }
 }
