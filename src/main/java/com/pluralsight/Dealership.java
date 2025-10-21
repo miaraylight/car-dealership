e23c0607b746b6106fe4abc5a8e11295d2808ec5
miaraylight <miaraylight@gmail.com>
2025-10-21 14:53:44 -0500

    add Dealership.java

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,55 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dealership {
+    private String name ;
+    private String address;
+    private String phone;
+
+    private ArrayList<Vehicle> inventory = new ArrayList<>();
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+//    public List<Vehicle> getVehiclesByPrice (double min, double max) {
+//
+//    }
+//
+//    public List<Vehicle> getVehiclesByMakeModel (String make, String model) {
+//
+//    }
+//
+//    public List<Vehicle> getVehiclesByYear (int minYear, int maxYear) {
+//
+//    }
+//
+//    public List<Vehicle> getVehiclesByColor (String color) {
+//
+//    }
+//
+//    public List<Vehicle> getVehiclesByMileage (int minMiles, int maxMiles) {
+//
+//    }
+//
+//    public List<Vehicle> getVehiclesByType (String vehicleType) {
+//
+//    }
+//
+//    public List<Vehicle> getAllVehicles () {
+//
+//    }
+
+    public void addVehicle (Vehicle vehicle) {
+
+    }
+
+    public void removeVehicle (Vehicle vehicle) {
+
+    }
+
 }
--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,14 +1,14 @@
 package com.pluralsight;
 
 public class Vehicle {
-    public int vin;
-    public int year;
-    public String make;
-    public String model;
-    public String vehicleType;
-    public String color;
-    public int odometer;
-    public double price;
+    private int vin;
+    private int year;
+    private String make;
+    private String model;
+    private String vehicleType;
+    private String color;
+    private int odometer;
+    private double price;
 
     public Vehicle(int vin, int year, String make, String model, String vehicleType, String color, int odometer, double price) {
         this.vin = vin;
