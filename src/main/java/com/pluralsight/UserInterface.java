d91d259ae055a86fc632bf65f31e927bd75852bf
miaraylight <miaraylight@gmail.com>
2025-10-24 11:01:46 -0500

    add UserInterface.java with empty methods

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,27 @@
 package com.pluralsight;
 
+import static com.pluralsight.DealershipFileManager.getDealership;
+
 public class UserInterface {
+    private Dealership dealership;
+
+    public void display() {}
+
+    public void processGetByPriceRequest() {}
+
+    public void processGetByMakeModelRequest() {}
+
+    public void processGetByYearRequest() {}
+
+    public void processGetByColorRequest() {}
+
+    public void processGetByMileageRequest() {}
+
+    public void processGetByVehicleTypeRequest() {}
+
+    public void processGetAllVehiclesRequest() {}
+
+    public void processAddVehicleRequest() {}
+
+    public void processRemoveVehicleRequest() {}
 }
